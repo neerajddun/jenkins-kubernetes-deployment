613e6fa0d4a2dae6fbf3e036ec4624da79add62b
unknown <neerajbijalwan96@gmail.com>
2026-05-27 13:41:56 +0530

    add gitignore files, update jenkinsfile and terraform

--- demo/terraform/output.tf
+++ demo/terraform/output.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_name" {
-    value = aws_s3_bucket.terraform_state.bucket
+  value = aws_s3_bucket.my-bucket
 }
 
 output "s3_bucket_arn" {
-    value = aws_s3_bucket.terraform_state.arn
+  value = aws_s3_bucket.my-bucket.arn
 }
